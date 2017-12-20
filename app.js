6c31cea0aee69304b71773187630f7961466c722
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2017-12-20 11:37:16 -0700

    Update test data

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('were??');
+const apiURL = "http://localhost:3000/";
+
+fetch(apiURL)
+    .then(response => response.json())
+    .then(console.log)
+    .catch(console.error);
+
+function postWolf() {
+    fetch(apiURL, {
+        method: "POST",
+        body: JSON.stringify({
+            data: "something"
+        }),
+        headers: new Headers ({
+            "Content-Type": "application/json"
+        })
+    })
+        .then(response => response.json())
+        .then(console.log)
+        .catch(console.error);
+}
+
+postWolf();
